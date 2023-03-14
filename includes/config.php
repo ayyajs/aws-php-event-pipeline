6f575344bc7d6f17812c07d9f73974d50f8c8dbc
Ayyadurai S <ayyajs.tam@gmail.com>
2023-03-14 15:30:39 +0530

    db updated

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,13 @@
 <?php 
-$global_config = array();
+    $global_config = array();
 
-$global_config["SiteGlobalPath"]    =   "http://localhost:8000";
-$global_config["SiteLocalPath"]     =   '/var/www/html/';
-$global_config["SiteUploadPath"]	=	$global_config["SiteLocalPath"].'uploads/';    	  	
+    $global_config["SiteGlobalPath"]    =   "http://localhost:8000";
+    $global_config["SiteLocalPath"]     =   '/var/www/html/';
+    $global_config["SiteUploadPath"]	=	$global_config["SiteLocalPath"].'uploads/';    	  	
 
-$global_config["DBHost"]            =   "db";
-$global_config["DBUserName"]        =   "newuser"; 
-$global_config["DBPassword"]        =   "newuser123";
-$global_config["DBDatabaseName"]    =   "event";
-$global_config["DBTablePrefix"]     =   "tbl_";
+    $global_config["DBHost"]            =   "localhost";
+    $global_config["DBUserName"]        =   "root"; 
+    $global_config["DBPassword"]        =   "dmb8)USTTVTyK4Zifzr(}#NZ]v<-<n";
+    $global_config["DBDatabaseName"]    =   "event-db";
+    $global_config["DBTablePrefix"]     =   "tbl_";
 ?>
